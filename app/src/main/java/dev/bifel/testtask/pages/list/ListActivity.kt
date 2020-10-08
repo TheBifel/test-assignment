62ad892c1358cf88d47727b14e6ec97cd75cde8c
Bohdan Ishchenko <thebifel345@gmail.com>
2020-10-08 17:47:22 +0300

    initial commit

---
+++ app/src/main/java/dev/bifel/testtask/pages/list/ListActivity.kt
@@ -0,0 +1,12 @@
+package dev.bifel.testtask.pages.list
+
+import android.app.Activity
+import android.os.Bundle
+import dev.bifel.testtask.R
+
+class ListActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
---
+++ app/src/main/java/dev/bifel/testtask/pages/person/PersonActivity.kt
@@ -0,0 +1,13 @@
+package dev.bifel.testtask.pages.person
+
+import android.app.Activity
+import android.os.Bundle
+import dev.bifel.testtask.R
+
+class PersonActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person)
+    }
+}
